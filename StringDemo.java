b2fcde9c37f979d5035de7871eb94e8cacbe9713
NeHaa <bhandarineha2015@gmail.com>
2018-07-31 07:02:38 +0545

    added project

---
+++ StringDemo.java
@@ -0,0 +1,7 @@
+public class StringDemo{
+   public static void main(String[]args){
+   String name ="Neha Bhandari Chetrii";
+   String s =name.substring(5,13);
+   System.out.println(s);
+     }
+   }
